30f876b8a4e78ad135b0659d13f3d7ca7da8aff7
Silvafox76 <ryan.w.dear@gmail.com>
2025-04-26 16:07:42 -0400

    Complete Conference Event Planner project setup

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,14 +4,41 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    {
+      img: "https://pixabay.com/images/download/lunch-1638188_640.jpg",
+      name: "Standard Meal",
+      cost: 20,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/images/download/salad-2068220_640.jpg",
+      name: "Vegetarian Meal",
+      cost: 18,
+      quantity: 0,
+    },
+    {
+      img: "https://pixabay.com/images/download/breakfast-1804436_640.jpg",
+      name: "Breakfast Meal",
+      cost: 15,
+      quantity: 0,
+    },
   ],
   reducers: {
-    toggleMealSelection: (state, action) => {
+    incrementMealQuantity: (state, action) => {
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
+    },
+    decrementMealQuantity: (state, action) => {
+      const item = state[action.payload];
+      if (item && item.quantity > 0) {
+        item.quantity--;
+      }
     },
   },
 });
 
-export const { toggleMealSelection } = mealsSlice.actions;
+export const { incrementMealQuantity, decrementMealQuantity } = mealsSlice.actions;
 
 export default mealsSlice.reducer;
